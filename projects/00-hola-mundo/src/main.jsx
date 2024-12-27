a580ce220b7d886df4beed0d3c336d7bb6b7a894
DJ190502 <djaguiar13@gmail.com>
2024-12-27 15:20:33 -0500

    modified:   projects/00-hola-mundo/src/main.jsx

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -3,26 +3,32 @@ import ReactDom from "react-dom/client";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-root.render(
-  <React.Fragment>
+const createButton = () => {
+  return (
     <button>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         stroke="currentColor"
-        class="size-6"
+        className="size-6"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
         />
       </svg>
       Hola Button
     </button>
-    <button>Hola Button</button>
-    <button>Hola Button</button>
+  );
+};
+
+root.render(
+  <React.Fragment>
+    {createButton()}
+    {createButton()}
+    {createButton()}
   </React.Fragment>
 );
