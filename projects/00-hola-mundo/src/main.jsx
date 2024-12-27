9865d25a27dd9234e0d4e6e4d7450855dcf4a295
DJ190502 <djaguiar13@gmail.com>
2024-12-27 15:41:14 -0500

    new file:   projects/00-hola-mundo/src/App.jsx 	modified:   projects/00-hola-mundo/src/main.jsx

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,34 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import {App} from '../src/App.jsx'
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-const Button = ({ text }) => {
-  return (
-    <button>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        stroke="currentColor"
-        className="size-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-        />
-      </svg>
-      {text}
-    </button>
-  );
-};
 
 root.render(
-  <React.Fragment>
-    <Button text="Boton1"></Button>
-    <Button text="Boton2"></Button>
-    <Button text="Boton3"></Button>
-  </React.Fragment>
+  <App/>
 );
