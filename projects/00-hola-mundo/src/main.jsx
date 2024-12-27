f21cf512714c3dd40eb9769f2379b276189dc0b2
DJ190502 <djaguiar13@gmail.com>
2024-12-27 15:09:27 -0500

    modified:   projects/00-hola-mundo/src/main.jsx

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import ReactDom from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDom from "react-dom/client";
 
-const root = createRoot(document.getElementById('root')).render(
+const root = ReactDom.createRoot(document.getElementById("root"))
 
-)
+root.render("Hola Mundo");
