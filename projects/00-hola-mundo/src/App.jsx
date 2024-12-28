b3f5d1d090ace482b537351a15db48ebfaf15ea1
Daniel Jesus <djaguiar13@gmail.com>
2024-12-27 23:09:11 -0500

    new file:   projects/00-hola-mundo/src/App.css 	modified:   projects/00-hola-mundo/src/App.jsx 	new file:   projects/00-hola-mundo/src/index.css 	modified:   projects/00-hola-mundo/src/main.jsx

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,9 +1,21 @@
 export function App(){
   return (
-    <article>
-      <header>
-        
+    <article className="tw-followCard">
+      <header className="hd-tw-followCard">
+        <img 
+        className="avatar-tw-followCard"
+        alt="Avatar" 
+        src="https://unavatar.io/github/DJ190502" />
+        <div>
+          <strong>Daniel Jesus Avila</strong>
+          <span>@DJ190502</span>
+        </div> 
       </header>
+      <aside>
+        <button>
+          Seguir
+        </button>
+      </aside>
     </article>
   )  
 }
