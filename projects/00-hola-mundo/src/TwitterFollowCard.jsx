6f03fd471a5caaee5f02bd1cc52b24390f5aafa9
Daniel Jesus <djaguiar13@gmail.com>
2024-12-28 17:20:00 -0500

    modified:   projects/00-hola-mundo/src/TwitterFollowCard.jsx

--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 //Podemos poner parametros para cuando no se pasa ningun parametro
-export function TwitterFollowCard({ formatedUserName, photo, username, name, isFollowing }) {
+export function TwitterFollowCard({ formatedUserName, photo, username, name}) {
+  const [isFollowing, setIsFollowing] = useState(false)
+
   const follow = isFollowing ? 'Siguiendo' : 'Seguir'
   const buttonClassName = isFollowing ? 'btn-tw-followCard is-following' : 'btn-tw-followCard'
+
+  const handleClick = () => {setIsFollowing(!isFollowing)} //Cambia el valor del boton
   return (
     <article className="tw-followCard">
       <header className="hd-tw-followCard">
@@ -17,7 +22,7 @@ export function TwitterFollowCard({ formatedUserName, photo, username, name, isF
         </div>
       </header>
       <aside className="aside-tw-followCard">
-       <button className={buttonClassName}>{follow}</button>
+       <button className={buttonClassName} onClick={handleClick}>{follow}</button>
       </aside>
     </article>
   );
