291b3401c1d11fb4ba4ff45cbfa367dad741446d
Daniel Jesus <djaguiar13@gmail.com>
2024-12-31 11:30:04 -0500

    modified:   projects/00-hola-mundo/src/App.jsx 	modified:   projects/00-hola-mundo/src/TwitterFollowCard.jsx

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,28 +1,43 @@
 import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
+
+const users = [
+  {
+    username: 'DJ190502',
+    name: 'Daniel Jesus Avila Aguiar',
+    isFollowing: true
+  },
+  {
+    username: 'midudev',
+    name: 'Miguel Angel Duran',
+    isFollowing: false,
+  },
+  {
+    username: 'pheralb',
+    name: 'Pablo H.',
+    isFollowing: true,
+  },
+  {
+    username: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false,
+  }
+];
 export function App() {
-  const formatedUserName = (username) => <span>{`@${username}`}</span>
+  const formatedUserName = (username) => <span>{`@${username}`}</span>;
   return (
     <section className="App">
-      <TwitterFollowCard
-        formatedUserName={formatedUserName("DJ190502")}
-        name="Daniel Jesus Avila Aguiar"
-        photo="DJ190502"
-        isFollowing
-      />
-      <TwitterFollowCard
-        formatedUserName={formatedUserName("Eduardo1306")}
-        name="Eduardo"
-        photo="Eduardo1306"
-        isFollowing={false}
-      />
-      <TwitterFollowCard
-        formatedUserName={formatedUserName("Dubrocq")}
-        name="Dubrocq"
-        photo="Dubrocq"
-        isFollowing
-      />
-      
+      {
+        users.map(({username, name, isFollowing}) => (
+            <TwitterFollowCard 
+            key={username}
+            username={formatedUserName(username)} 
+            initialIsFollowing={isFollowing} 
+            photo={username}>
+            <strong>{name}</strong>
+            </TwitterFollowCard>
+          ))
+      }
     </section>
-  )
+  );
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 //Podemos poner parametros para cuando no se pasa ningun parametro
-export function TwitterFollowCard({ formatedUserName, photo, username, name}) {
-  const [isFollowing, setIsFollowing] = useState(false)
+export function TwitterFollowCard({ children, photo, username, name, initialIsFollowing}) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
 
   const follow = isFollowing ? 'Siguiendo' : 'Seguir'
   const buttonClassName = isFollowing ? 'btn-tw-followCard is-following' : 'btn-tw-followCard'
@@ -17,8 +17,8 @@ export function TwitterFollowCard({ formatedUserName, photo, username, name}) {
           src={`https://unavatar.io/github/${photo}`}
         />
         <div className="div-tw-followCard">
-          <strong className="name-tw-followCard">{name}</strong>
-          <span className="user-tw-followCard">{formatedUserName}</span>
+          {children}
+          <span className="user-tw-followCard">{username}</span>
         </div>
       </header>
       <aside className="aside-tw-followCard">
